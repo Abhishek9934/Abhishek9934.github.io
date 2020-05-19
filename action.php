59d85ec13d9dccd38c27ed89e0f7c10e66f68760
Abhishek9934 <abhiigupta9934@gmail.com>
2020-05-19 08:41:25 +0530

    form php

---
+++ action.php
@@ -0,0 +1,17 @@
+<<?php 
+
+$name = $_POST['Name'];
+$email= $_POST['Email'];
+$subject = $_POST['Subject'];
+
+$message = $_POST['Message'];
+$recipient= "abhiigupta9934@gmail.com";
+$content=  "From : $name \n Message: $message";
+
+$mailheader = "From: $email \r\n";
+
+mail($recipient,$subject,$content , $mailheader) or die("Error");
+echo "Thank You.\n Your message has been sent.";
+
+
+ ?>
